f28fa94b84c415cfd53782bd2b35e489eb58d4c1
Nguyen Anh Khoa <khoacoi1986@gmail.com>
2013-05-31 19:06:23 +0700

    Some update for Chat

--- Web/Scripts/Modules/Chat/Chat.js
+++ Web/Scripts/Modules/Chat/Chat.js
@@ -1,33 +1,62 @@
 ﻿function ChatViewModelFuntion(options) {
     self = ChatViewModel;
     self.chatMessage = ko.observable();
+    var chat = $.connection.chatHub;
 
-    ko.applyBindings(self);
+    self.chatMessageKeyDown = function (viewModel, event) {
+        
+        if (event.keyCode == 13) {
+            self.sendMessage();
+        }
+        return true;
+
+    }
+    self.sendMessage = function () {
+        if (self.chatMessage() != "") {
+            // Call the Send method on the hub. 
+            chat.server.send(self.chatMessage());
+            // Clear text box and reset focus for next comment. 
+            self.chatMessage("");
+        }
+        $('#message').focus();
+    }
+
+    chat.client.addNewMessageToPage = function (name, message) {
+        // Add the message to the page. 
+        $('#discussion').append('<li><strong>' + htmlEncode(name)
+            + '</strong>: ' + htmlEncode(message) + '</li>');
+    };
+
+    $.connection.hub.start().done(function () {
+        self.sendMessage();
+    });
+
+    
     
     // SignalR Chat Hub
     //SignalR script to update the chat page and send messages.
-    $(function () {
-        var chat = $.connection.chatHub;
+    //$(function () {
+    //    var chat = $.connection.chatHub;
 
-        chat.client.addNewMessageToPage = function (name, message) {
-            // Add the message to the page. 
-            $('#discussion').append('<li><strong>' + htmlEncode(name)
-                + '</strong>: ' + htmlEncode(message) + '</li>');
-        };
-        
+    //    chat.client.addNewMessageToPage = function (name, message) {
+    //        // Add the message to the page. 
+    //        $('#discussion').append('<li><strong>' + htmlEncode(name)
+    //            + '</strong>: ' + htmlEncode(message) + '</li>');
+    //    };
 
-        $.connection.hub.start().done(function () {
-            $('#sendmessage').click(function () {
-                if (self.chatMessage() != "") {
-                    // Call the Send method on the hub. 
-                    chat.server.send(self.chatMessage());
-                    // Clear text box and reset focus for next comment. 
-                    self.chatMessage("");
-                }
-                $('#message').focus();
-            });
-        });
-    });
+
+    //    $.connection.hub.start().done(function () {
+    //        $('#sendmessage').click(function () {
+    //            if (self.chatMessage() != "") {
+    //                // Call the Send method on the hub. 
+    //                chat.server.send(self.chatMessage());
+    //                // Clear text box and reset focus for next comment. 
+    //                self.chatMessage("");
+    //            }
+    //            $('#message').focus();
+    //        });
+    //    });
+    //});
 
     function htmlEncode(value) {
         var encodedValue = $('<div />').text(value).html();
@@ -35,6 +64,7 @@
     }
     // End ChatScript
 
+    ko.applyBindings(self);
 
     // Set initial focus to message input box. 
     $('#message').focus();
