cb26f7fd09c74d326c7a9547af6b0da0ae60c95a
Nguyen Anh Khoa <khoacoi1986@gmail.com>
2013-06-01 23:40:07 +0700

    add scrollbar

--- Web/Scripts/Modules/Chat/Chat.js
+++ Web/Scripts/Modules/Chat/Chat.js
@@ -15,6 +15,7 @@
         if (self.chatMessage() != "") {
             // Call the Send method on the hub. 
             chat.server.send(self.chatMessage());
+            autoScrollToBottom();
             // Clear text box and reset focus for next comment. 
             self.chatMessage("");
         }
@@ -32,31 +33,16 @@
     });
 
     
+    //scroll bar
+    var scrollPanel = $('#discussionpanel');
+    function autoScrollToBottom() {
+        if (scrollPanel.get(0).scrollHeight > scrollPanel.height()) {
+            if (scrollPanel.outerHeight() == (scrollPanel.get(0).scrollHeight - scrollPanel.scrollTop())) {
+                scrollPanel.scrollTop(scrollPanel.prop("scrollHeight"));
+            }
+        }
+    }
     
-    // SignalR Chat Hub
-    //SignalR script to update the chat page and send messages.
-    //$(function () {
-    //    var chat = $.connection.chatHub;
-
-    //    chat.client.addNewMessageToPage = function (name, message) {
-    //        // Add the message to the page. 
-    //        $('#discussion').append('<li><strong>' + htmlEncode(name)
-    //            + '</strong>: ' + htmlEncode(message) + '</li>');
-    //    };
-
-
-    //    $.connection.hub.start().done(function () {
-    //        $('#sendmessage').click(function () {
-    //            if (self.chatMessage() != "") {
-    //                // Call the Send method on the hub. 
-    //                chat.server.send(self.chatMessage());
-    //                // Clear text box and reset focus for next comment. 
-    //                self.chatMessage("");
-    //            }
-    //            $('#message').focus();
-    //        });
-    //    });
-    //});
 
     function htmlEncode(value) {
         var encodedValue = $('<div />').text(value).html();
--- Web/Startup/Tasks/BundleRegistrationTask.cs
+++ Web/Startup/Tasks/BundleRegistrationTask.cs
@@ -19,7 +19,9 @@ namespace Web.Startup.Tasks
 
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/plugins/jquery/jquery-{version}.js",
-                        "~/Scripts/plugins/jquery/jquery.metadata.js"));
+                        "~/Scripts/plugins/jquery/jquery.metadata.js",
+                        "~/Scripts/plugins/jquery/jquery.jscrollpane.js",
+                        "~/Scripts/plugins/jquery/jquery.mousewheel.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
@@ -65,65 +67,10 @@ namespace Web.Startup.Tasks
                         "~/Content/themes/base/jquery.ui.progressbar.css",
                         "~/Content/themes/base/jquery.ui.theme.css"));
 
+            bundles.Add(new StyleBundle("~/Content/system/bundle/jqueryui").Include(
+                "~/Content/system/css/jquery.jscrollpane.css"
+                ));
 
-            
-
-            ////BundleTable.EnableOptimizations = true;
-
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-            //            "~/Scripts/plugins/jquery/jquery-ui-{version}.js",
-            //            "~/Scripts/plugins/jquery/jquery.jscrollpane.js",
-            //            "~/Scripts/plugins/jquery/jquery.form.js",
-            //            "~/Scripts/plugins/jquery/jquery.tree.js",
-            //            "~/Scripts/plugins/jquery/jquery.uniform.js"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/extension")
-            //    .IncludeDirectory("~/Scripts/Extensions", "*.js"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/jqueryvalidation").Include(
-            //            "~/Scripts/plugins/jquery/jquery.unobtrusive*",
-            //            "~/Scripts/plugins/jquery/jquery.validate*"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/jquerycontrol").Include(
-            //                "~/Scripts/plugins/jquery/jquery.placeholder.js",
-            //                "~/Scripts/plugins/jquery/jquery.maskedinput-1.3.js",
-            //                "~/Scripts/plugins/jquery/autoNumeric-{version}.js",
-            //                "~/Scripts/plugins/jquery/jquery-ui-timepicker-addon.js",
-            //                "~/Scripts/plugins/jquery/jquery.multiselect.js",
-            //                "~/Scripts/plugins/jquery/jquery.autoellipsis.js",
-            //                "~/Scripts/plugins/jquery/jquery.numberformatter-1.2.3.js"
-            //                ));
-
-
-            //bundles.Add(new ScriptBundle("~/bundles/headjs").IncludeDirectory(
-            //            "~/Scripts/plugins/headjs", "*.js"));
-
-            //bundles.Add(new ScriptBundle("~/bundles/common").Include(
-            //            "~/Scripts/Controllers/Common/messages.js",
-            //            "~/Scripts/Controllers/Common/controls.js",
-            //            "~/Scripts/Controllers/Common/table.js",
-            //            "~/Scripts/Controllers/Common/dialog.js",
-            //            "~/Scripts/Controllers/Common/init.js",
-            //            "~/Scripts/Controllers/Common/helpers.js"));
-
-            //bundles.Add(new StyleBundle("~/Content/css/bootstrap").Include(
-            //            "~/Content/plugins/bootstrap/css/bootstrap.grid.css"
-            //    ));
-
-            //bundles.Add(new StyleBundle("~/Content/system/bundle/jqueryui").Include(
-            //            "~/Content/system/css/jquery-ui-1.9.2.custom.css",
-            //            "~/Content/system/css/jquery-ui-timepicker-addon.css",
-            //            "~/Content/system/css/jquery.multiselect.uniform.css",
-            //            "~/Content/system/css/jqtree.css",
-            //            "~/Content/system/css/jquery.jscrollpane.css",
-            //            "~/Content/system/css/uniform.default.css"));
-
-            //var styleBundle = new StyleBundle("~/Content/system/bundle/site").Include(
-            //            "~/Content/system/css/common.css",
-            //            "~/Content/system/css/layout.css");
-            //styleBundle.Transforms.Insert(0, new CssFixPieHtcPathTransform());
-
-            //bundles.Add(styleBundle);
         }
     }
 }
